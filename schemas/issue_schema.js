b10955acbe96bfca691d8de8f5647ff6edad0c63
Anhelina Kulkova <angeltutova@gmail.com>
2024-04-06 17:32:13 +0200

    Fixed the non required fields of an issue

--- schemas/issue_schema.js
+++ schemas/issue_schema.js
@@ -4,9 +4,9 @@ module.exports.IssueSchema = Joi.object({
   issue: Joi.object({
     name: Joi.string().required(),
     completed: Joi.boolean(),
-    details: Joi.string(), 
-    priority: Joi.string(), // add rules for priority
-    time_start: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/), 
-    time_end: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/)
+    details: Joi.string().allow(''), 
+    priority: Joi.string().allow(''), // add rules for priority
+    time_start: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/).allow(''), 
+    time_end: Joi.string().regex(/^([0-9]{2})\:([0-9]{2})$/).allow('')
   }).required(),
 });
